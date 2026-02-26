b0cb52bb8dd07efa4eecc61e85577f8afdbdd6cc
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2026-02-26 18:02:20 +0200

    refactor: replace Link with NavLink for main navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import cn from 'classnames';
 
+const handleLinkClass = ({ isActive }: { isActive: boolean }) =>
+  cn('navbar-item', { 'is-active': isActive });
+
 export const App = () => {
-  const location = useLocation();
-
-  const isHome = location.pathname === '/';
-  const isTabs = location.pathname.startsWith('/tabs');
-
   return (
     <>
       <nav
@@ -18,16 +16,13 @@ export const App = () => {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className={cn('navbar-item', { 'is-active': isHome })}>
+            <NavLink to="/" className={handleLinkClass}>
               Home
-            </Link>
+            </NavLink>
 
-            <Link
-              to="/tabs"
-              className={cn('navbar-item', { 'is-active': isTabs })}
-            >
+            <NavLink to="/tabs" className={handleLinkClass}>
               Tabs
-            </Link>
+            </NavLink>
           </div>
         </div>
       </nav>
@@ -38,3 +33,46 @@ export const App = () => {
     </>
   );
 };
+
+// import 'bulma/css/bulma.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
+// import './App.scss';
+// import { Link, Outlet, useLocation } from 'react-router-dom';
+// import cn from 'classnames';
+
+// export const App = () => {
+//   const location = useLocation();
+
+//   const isHome = location.pathname === '/';
+//   const isTabs = location.pathname.startsWith('/tabs');
+
+//   return (
+//     <>
+//       <nav
+//         className="navbar is-light is-fixed-top is-mobile has-shadow"
+//         data-cy="Nav"
+//       >
+//         <div className="container">
+//           <div className="navbar-brand">
+//             <Link to="/" className={cn('navbar-item', { 'is-active': isHome })}>
+//               Home
+//             </Link>
+
+//             <Link
+//               to="/tabs"
+//               className={cn('navbar-item', { 'is-active': isTabs })}
+//             >
+//               Tabs
+//             </Link>
+//           </div>
+//         </div>
+//       </nav>
+
+//       <div className="section">
+//         <Outlet />
+//       </div>
+//     </>
+//   );
+// };
+
+// // Ограничение на не использовать NavLink было только для вкладок (Tabs), где is-active добавляется к <li>.
